6adcd4e0007a1a5fea47d24bc4abf2bcd85255a3
Patais <loanbillonnet@gmail.com>
2022-03-31 18:23:43 +0200

    upgrade added user with conditionals

--- routes/users.js
+++ routes/users.js
@@ -28,14 +28,32 @@ router.get('/', (req, res) => {
     res.send(users);
 });
 
-// create an user
+// create user
 router.post('/', (req, res) => {
     let user = req.body;
-    users.push(user);
-    res.send("User added");
+    let missingArguments = []
+    if (typeof user.username === 'undefined') {
+        missingArguments.push("username");
+    }
+    if (typeof user.mail === 'undefined') {
+        missingArguments.push("mail");
+    }
+    if (typeof user.age === 'undefined') {
+        missingArguments.push("age");
+    }
+    if (typeof user.active === 'undefined') {
+        missingArguments.push("active");
+    }
+
+    if (missingArguments.length !== 0) {
+        res.send(`Missing arguments : ${missingArguments}`)
+    } else {
+        users.push(user);
+        res.send("User added");
+    }
 });
 
-// delete an user
+// delete user
 router.delete('/', (req, res) => {
     let user_send = req.body;
     let index = users.findIndex(user => user.username === user_send.username);
