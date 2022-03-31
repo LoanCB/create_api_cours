eaf6cfd0684fb44d4bce9d72512f3bb99fefac40
Patais <loanbillonnet@gmail.com>
2022-03-31 16:19:38 +0200

    create new request post for add an user

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import usersRoutes from "./routes/users.js"
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 5000;
 
+app.use(bodyParser.json());
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -4,27 +4,33 @@ const router = express.Router();
 
 let users = [
     {
-        username: "Loan",
-        mail: "loanbillonnet@gmail.com",
-        age: 19,
-        active: true
+        "username": "Loan",
+        "mail": "loanbillonnet@gmail.com",
+        "age": 19,
+        "active": true
     },
     {
-        username: "Romain",
-        mail: "romain.gojard@gmail.com",
-        age: 21,
-        active: true
+        "username": "Romain",
+        "mail": "romain.gojard@gmail.com",
+        "age": 21,
+        "active": true
     },
     {
-        username: "Noé",
-        mail: "noe@macoley.fr",
-        age: 20,
-        active: false
+        "username": "Noé",
+        "mail": "noe@macoley.fr",
+        "age": 20,
+        "active": false
     }
 ];
 
 router.get('/', (req, res) => {
     res.send(users);
+});
+
+router.post('/', (req, res) => {
+    let user = req.body;
+    users.push(user);
+    res.send("User added");
 });
 
 export default router;
