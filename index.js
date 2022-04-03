e40a1f790b295fa2bd4a0550f4d4d0ae290c8c26
Loan <loanbillonnet@gmail.com>
2022-04-03 20:10:39 +0200

    add authorizations

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ import express from "express";
 import usersRoutes from "./routes/users.js"
 import articlesRoutes from "./routes/articles.js";
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import {JWT_SIGN_SECRET} from "./data.js";
 
 const app = express();
 const port = 5000;
+const logger = function (req, res, next) {
+    if (req.method !== 'GET') {
+        const auth = req.header('authorization');
+        if (auth) {
+            try {
+                jwt.verify(auth.replace('Bearer ', ''), JWT_SIGN_SECRET);
+                next();
+            } catch (err) {
+                res.send(err.name);
+            }
+        } else {
+            res.send("Need a bearer token to use this route. You can get it on login (refer to doc)");
+        }
+    } else {
+        next();
+    }
+};
 
 app.use(bodyParser.json());
 app.use('/users', usersRoutes);
+app.use(logger);
 app.use('/articles', articlesRoutes);
 
 app.get('/', (req, res) => {
